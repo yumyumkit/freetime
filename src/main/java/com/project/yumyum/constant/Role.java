574d886de6096476f663a8544706fc617792840e
HyerimHwang <82012938+Hyerim926@users.noreply.github.com>
2021-11-26 11:23:54 +0900

    Delete src directory

--- src/main/java/com/project/yumyum/constant/Role.java
+++
@@ -1,5 +0,0 @@
-package com.project.yumyum.constant;
-
-public enum Role {
-    USER, ADMIN
-}
--- src/main/java/com/project/yumyum/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.yumyum.repository;
-
-import com.project.yumyum.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByEmail(String email);
-}
